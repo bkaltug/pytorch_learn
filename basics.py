1834447ec9f26a4ae28162a0dc1affdce48396ff
bkaltug <berkay.ustagul@gmail.com>
2025-11-01 12:47:21 +0300

    Constructed a basic Neural Network

--- basics.py
+++ basics.py
@@ -72,51 +72,81 @@ class NeuralNetwork(nn.Module):
         logits = self.linear_relu_stack(x)
         return logits
 
+# model = NeuralNetwork().to(device)
+# print(model)
+
+# # Optimizing the parameters
+
+# loss_func = nn.CrossEntropyLoss()
+# optimizer = torch.optim.SGD(model.parameters(),lr= 1e-3)
+
+# def train(dataloader, model, loss_func, optimizer):
+#     size = len(dataloader.dataset)
+#     model.train()
+#     for batch, (X, y) in enumerate(dataloader):
+#         X,y = X.to(device), y.to(device)
+
+#         # Prediction error
+#         pred = model(X)
+#         loss = loss_func(pred, y)
+
+#         # Backpropagation
+#         loss.backward()
+#         optimizer.step()
+#         optimizer.zero_grad()
+
+#         if batch % 100 == 0:
+#             loss, current = loss.item(), (batch + 1) * len(X)
+#             print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+# def test(dataloader, model, loss_func):
+#     size = len(dataloader.dataset)
+#     num_batches = len(dataloader)
+#     model.eval()
+#     test_loss, correct = 0,0
+#     with torch.no_grad():
+#         for X,y in dataloader:
+#             X,y = X.to(device), y.to(device)
+#             pred = model(X)
+#             test_loss += loss_func(pred,y).item()
+#             correct += (pred.argmax(1)== y).type(torch.float).sum().item()
+#     test_loss /= num_batches
+#     correct /= size
+#     print(f"Test error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+# epochs = 5
+# for t in range(epochs):
+#     print(f"Epoch{t+1}\n--------------------------------")
+#     train(train_dataloader,model,loss_func,optimizer)
+#     test(test_dataloader,model, loss_func)
+#     print("Done!") 
+
+# # Saving the trained model
+# torch.save(model.state_dict(),"model.pth")
+# print("Saved PyTorch model state to model.pth")
+
+# Loading the saved model
 model = NeuralNetwork().to(device)
-print(model)
+model.load_state_dict(torch.load("model.pth",weights_only=True))
 
-# Optimizing the parameters
+# Use the model to make predictions
+classes = [
+        "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
 
-loss_func = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(),lr= 1e-3)
-
-def train(dataloader, model, loss_func, optimizer):
-    size = len(dataloader.dataset)
-    model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        X,y = X.to(device), y.to(device)
-
-        # Prediction error
-        pred = model(X)
-        loss = loss_func(pred, y)
-
-        # Backpropagation
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        if batch % 100 == 0:
-            loss, current = loss.item(), (batch + 1) * len(X)
-            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
-
-def test(dataloader, model, loss_func):
-    size = len(dataloader.dataset)
-    num_batches = len(dataloader)
-    model.eval()
-    test_loss, correct = 0,0
-    with torch.no_grad():
-        for X,y in dataloader:
-            X,y = X.to(device), y.to(device)
-            pred = model(X)
-            test_loss += loss_func(pred,y).item()
-            correct += (pred.argmax(1)== y).type(torch.float).sum().item()
-    test_loss /= num_batches
-    correct /= size
-    print(f"Test error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
-
-epochs = 5
-for t in range(epochs):
-    print(f"Epoch{t+1}\n--------------------------------")
-    train(train_dataloader,model,loss_func,optimizer)
-    test(test_dataloader,model, loss_func)
-    print("Done!") 
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f"Predicted: {predicted}, Actual: {actual}")
