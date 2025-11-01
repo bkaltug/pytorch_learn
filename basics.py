ab0d27288229592a6d798570c3b3f259f6948ba2
bkaltug <berkay.ustagul@gmail.com>
2025-11-01 12:30:36 +0300

    Optimizing parameters

--- basics.py
+++ basics.py
@@ -74,3 +74,49 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork().to(device)
 print(model)
+
+# Optimizing the parameters
+
+loss_func = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(),lr= 1e-3)
+
+def train(dataloader, model, loss_func, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+        X,y = X.to(device), y.to(device)
+
+        # Prediction error
+        pred = model(X)
+        loss = loss_func(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f} [{current:>5d}/{size:>5d}]")
+
+def test(dataloader, model, loss_func):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0,0
+    with torch.no_grad():
+        for X,y in dataloader:
+            X,y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_func(pred,y).item()
+            correct += (pred.argmax(1)== y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch{t+1}\n--------------------------------")
+    train(train_dataloader,model,loss_func,optimizer)
+    test(test_dataloader,model, loss_func)
+    print("Done!") 
